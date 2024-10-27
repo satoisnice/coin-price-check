ab652f54acd003789d6b8c006faae133335510d2
connor <connorbell015@gmail.com>
2024-10-26 21:03:56 -0700

    got the websocket stream up and running but not sure how to format data, added the library max was using maybe will be helpful

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ coins = ['BTCUSDT', 'ETHUSDT', 'XMRUSDT'] #coins to price check
 coins = [coin.lower() + '@kline_3m' for coin in coins] #add kline to get the candle
 
 coins = '/'.join(coins)
-
-socket = f"wss://data-stream.binance.vision:443/ws{coins}"
-def on_message(socket, msg):
+print(coins)
+socket = f"wss://data-stream.binance.vision:443/ws/{coins}"
+def on_message(ws, msg):
     msg = json.loads(msg)
     print(msg)
+
+ws = websocket.WebSocketApp(socket, on_message=on_message)
+ws.run_forever()
