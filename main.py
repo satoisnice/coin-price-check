32a339a63441651559602536687e5d474a1cc165
connor <connorbell015@gmail.com>
2024-10-26 20:49:30 -0700

    trouble w/ websocket

--- main.py
+++ main.py
@@ -6,4 +6,8 @@ coins = ['BTCUSDT', 'ETHUSDT', 'XMRUSDT'] #coins to price check
 coins = [coin.lower() + '@kline_3m' for coin in coins] #add kline to get the candle
 
 coins = '/'.join(coins)
-print(coins)
+
+socket = f"wss://data-stream.binance.vision:443/ws{coins}"
+def on_message(socket, msg):
+    msg = json.loads(msg)
+    print(msg)
